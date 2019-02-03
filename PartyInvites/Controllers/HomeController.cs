fefa9b4b44e6c0759c65beba6c4d2faf4a8fd85a
Taki <Taki.Hicham.01@outlook.com>
2019-02-03 23:41:42 +0100

    Adding Models/Controllers/Views and configuring EF Core CLI

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+
+        public HomeController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Respond()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Respond(GuestResponse response)
+        {
+            context.Responses.Add(response);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Thanks), new {Name = response.Name, WillAttend = response.WillAttend});
+        }
+
+        public IActionResult Thanks(GuestResponse response)
+        {
+            return View(response);
+        }
+
+        public IActionResult ListResponses()
+        {
+           return View(context.Responses.OrderByDescending(r => r.WillAttend));
+        }
+    }
+}
---
+++ PartyInvites/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PartyInvites.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GuestResponse> Responses { get; set; }
+        
+    }
+}
